dc386cdd92c1ae03e6abb7bf7074fc255d2edf7d
dzvision <5590839+dzvision@users.noreply.github.com>
2023-12-05 14:09:50 +0800

    update script
    update script to have option to choose

--- wg-change-port-firewalld.sh
+++ wg-change-port-firewalld.sh
@@ -1,51 +1,74 @@
 #!/bin/bash
 
-# Ask the user if they want to check the current WireGuard port
-read -p "Do you want to check the current WireGuard port? (y/n) " CHECK_PORT
+while true; do
+  # Display menu options
+  echo "Choose an option:"
+  echo "1) Check Current WireGuard Port"
+  echo "2) Change WireGuard Port"
+  echo "3) Exit"
 
-if [[ $CHECK_PORT =~ ^[Yy]$ ]]; then
-  # Get the current WireGuard port from the config file
-  WG_PORT=$(grep -oP '(?<=ListenPort = )\d+' /etc/wireguard/wg0.conf)
+  # Read user input
+  read -p "Enter your choice (1/2/3): " CHOICE
 
-  # Show the current WireGuard port
-  echo "The current WireGuard port is $WG_PORT."
-fi
+  case $CHOICE in
+    1)
+      # Check the current WireGuard port from the config file
+      WG_PORT=$(grep -oP '(?<=ListenPort = )\d+' /etc/wireguard/wg0.conf)
+      echo "The current WireGuard port is $WG_PORT."
+      ;;
+    2)
+      # Ask the user for the desired port
+      read -p "Enter the desired WireGuard port (leave blank for random): " NEW_PORT_INPUT
 
-# Ask the user if they want to generate a random port
-read -p "Do you want to generate a random WireGuard port? (y/n) " GENERATE_PORT
+      if [[ -z $NEW_PORT_INPUT ]]; then
+        # Generate a random port between 1024 and 65535
+        NEW_PORT=$(shuf -i 1024-65535 -n 1)
 
-if [[ $GENERATE_PORT =~ ^[Yy]$ ]]; then
-  # Generate a random port between 1024 and 65535
-  NEW_PORT=$(shuf -i 1024-65535 -n 1)
+        # Check if the new port is already in use by checking the firewall rule
+        while [[ $(firewall-cmd --list-ports | grep -w $NEW_PORT/udp) ]]; do
+          # Generate a new random port if the current one is already in use
+          NEW_PORT=$(shuf -i 1024-65535 -n 1)
+        done
 
-  # Check if the new port is already in use by checking the firewall rule
-  while [[ $(firewall-cmd --list-ports | grep -w $NEW_PORT/udp) ]]; do
-    # Generate a new random port if the current one is already in use
-    NEW_PORT=$(shuf -i 1024-65535 -n 1)
-  done
+        echo "Generated random port: $NEW_PORT"
+      else
+        # Use the user-provided port
+        NEW_PORT=$NEW_PORT_INPUT
+      fi
 
-  echo "Generated random port: $NEW_PORT"
+      # Check the current WireGuard port from the config file
+      WG_PORT=$(grep -oP '(?<=ListenPort = )\d+' /etc/wireguard/wg0.conf)
+      echo "The current WireGuard port is $WG_PORT."
 
-  # Get the current WireGuard port from the config file
-  WG_PORT=$(grep -oP '(?<=ListenPort = )\d+' /etc/wireguard/wg0.conf)
+      # Update the WireGuard config file with the new port
+      WG_CONFIG="/etc/wireguard/wg0.conf"
+      sed -i "s/ListenPort = $WG_PORT/ListenPort = $NEW_PORT/" $WG_CONFIG
 
-  # Update the WireGuard config file with the new port
-  WG_CONFIG="/etc/wireguard/wg0.conf"
-  sed -i "s/ListenPort = $WG_PORT/ListenPort = $NEW_PORT/" $WG_CONFIG
+      # Reload the WireGuard config
+      systemctl restart wg-quick@wg0.service
 
-  # Reload the WireGuard config
-  systemctl restart wg-quick@wg0.service
+      # Remove the old firewall rule
+      firewall-cmd --zone=public --remove-port=$WG_PORT/udp
 
-  # Remove the old firewall rule
-  firewall-cmd --zone=public --remove-port=$WG_PORT/udp
+      # Add the new firewall rule
+      firewall-cmd --zone=public --add-port=$NEW_PORT/udp
 
-  # Add the new firewall rule
-  firewall-cmd --zone=public --add-port=$NEW_PORT/udp
+      # Save the firewall configuration
+      firewall-cmd --runtime-to-permanent
 
-  # Save the firewall configuration
-  firewall-cmd --runtime-to-permanent
+      # Show the current firewall rule for the new port
+      echo "The current firewall rule for port is: (WG Port $NEW_PORT)"
+      firewall-cmd --list-ports | grep -w $NEW_PORT/udp
+      ;;
+    3)
+      # Exit the script
+      echo "Exiting..."
+      exit 0
+      ;;
+    *)
+      # Handle invalid input
+      echo "Invalid choice. Please enter 1, 2, or 3."
+      ;;
+  esac
+done
 
-  # Show the current firewall rule for the new port
-  echo "The current firewall rule for port $NEW_PORT is:"
-  firewall-cmd --list-ports | grep -w $NEW_PORT/udp
-fi
--- wg-change-port-ufw.sh
+++ wg-change-port-ufw.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# Get the current WireGuard port from the config file
-WG_PORT=$(grep -oP '(?<=ListenPort = )\d+' /etc/wireguard/wg0.conf)
+# Function to check and display the current WireGuard port
+check_current_port() {
+  WG_PORT=$(grep -oP '(?<=ListenPort = )\d+' /etc/wireguard/wg0.conf)
+  echo "The current WireGuard port is $WG_PORT."
+}
 
-# Show the current WireGuard port
-echo "The current WireGuard port is $WG_PORT."
+# Function to change the WireGuard port
+change_wireguard_port() {
+  check_current_port
 
-# Ask the user for the new port or press enter to generate a random port
-read -p "Enter the new WireGuard port or press Enter to generate a random port: " NEW_PORT
+  # Ask the user for the new port or press enter to generate a random port
+  read -p "Enter the new WireGuard port or press Enter to generate a random port: " NEW_PORT
 
-# If the user didn't enter a new port, generate a random port
-if [ -z "$NEW_PORT" ]; then
-  # Generate a random port between 1024 and 65535
-  NEW_PORT=$(shuf -i 1024-65535 -n 1)
-  echo "Generated random port: $NEW_PORT"
-fi
+  # If the user didn't enter a new port, generate a random port
+  if [ -z "$NEW_PORT" ]; then
+    # Generate a random port between 1024 and 65535
+    NEW_PORT=$(shuf -i 1024-65535 -n 1)
+    echo "Generated random port: $NEW_PORT"
+  fi
 
-# Update the WireGuard config file with the new port
-sed -i "s/ListenPort = $WG_PORT/ListenPort = $NEW_PORT/" /etc/wireguard/wg0.conf
+  # Update the WireGuard config file with the new port
+  sed -i "s/ListenPort = $WG_PORT/ListenPort = $NEW_PORT/" /etc/wireguard/wg0.conf
 
-# Reload the WireGuard config
-systemctl restart wg-quick@wg0.service
+  # Reload the WireGuard config
+  systemctl restart wg-quick@wg0.service
 
-# Ask the user if they want to update the firewall rule
-read -p "Do you want to update the firewall rule? (y/n) " UPDATE_FIREWALL
-
-if [[ $UPDATE_FIREWALL =~ ^[Yy]$ ]]; then
   # Remove the old firewall rule
   ufw delete allow $WG_PORT/udp
 
@@ -34,5 +34,33 @@ if [[ $UPDATE_FIREWALL =~ ^[Yy]$ ]]; then
 
   # Enable ufw
   ufw --force enable
-fi
-                                                                                                           37        1,1           All
+
+  # Display the current firewall status
+  ufw status
+}
+
+# Main menu
+while true; do
+  echo "WireGuard Configuration Menu:"
+  echo "1) Check Current Port"
+  echo "2) Change WireGuard Port"
+  echo "3) Exit"
+
+  read -p "Enter your choice (1-3): " CHOICE
+
+  case $CHOICE in
+    1)
+      check_current_port
+      ;;
+    2)
+      change_wireguard_port
+      ;;
+    3)
+      echo "Exiting WireGuard Configuration Menu."
+      exit 0
+      ;;
+    *)
+      echo "Invalid choice. Please enter a number between 1 and 3."
+      ;;
+  esac
+done
